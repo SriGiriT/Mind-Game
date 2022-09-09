1d56ac00cf18fe2548e3740acf2fdc760b1ceefb
SriGiriT <srigirit369@gmail.com>
2022-09-09 23:47:32 +0530

    Initialization of Game-06

---
+++ lib/screens/Game66.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class Game66 extends StatefulWidget {
+  const Game66({Key? key}) : super(key: key);
+
+  @override
+  State<Game66> createState() => _Game66State();
+}
+
+class _Game66State extends State<Game66> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Game66'),
+      ),
+      body: const Center(
+        child: Text('Game66'),
+      ),
+    );
+  }
+}
